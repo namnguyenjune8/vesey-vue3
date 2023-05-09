64e538ec85966aba797635f9309c32f7acae4680
Nguyen Hoang Nam <namnguyenjune8@gmail.com>
2023-05-09 23:29:31 +0700

    update header

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+  module.exports = {
+      mode: 'jit',
+      purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+      theme: {
+        
+      },
+      variants: {},
+      plugins: [],
+    }
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/scss/variable.scss";`
+      }
+    }
+  }
+};
